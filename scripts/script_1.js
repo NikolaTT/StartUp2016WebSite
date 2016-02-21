0776c5253ae96514792d1cefd0f5675b4c2077b5
NikolaTT <nekavsitam@gmail.com>
2016-02-21 11:10:24 +0200

    JS code changed to make elements hidden through visibility, not through display property. This workaround is needed cause Swiper doesn't work properly if display is = none;

--- scripts/script_1.js
+++ scripts/script_1.js
@@ -12,22 +12,34 @@ var unselectedTabBackgroundColor = "#EcEcEc";
 var unselectedTabTextColor = "#757575";
 
 
-window.onload = function () {
+//window.onload = function () {
 
-    var tabContents = document.getElementsByClassName(tabContentClass);
+var tabContents = document.getElementsByClassName(tabContentClass);
+for (var j = 0; j < tabContents.length; j++) {
+    tabContents[j].style.visibility = "hidden";
+}
+
+currentTab = "conference";
+initialTabSelector = "conference-tab-selector";
+
+document.getElementById(initialTabSelector).style.backgroundColor = selectedTabBackgroundColor;
+document.getElementById(initialTabSelector).style.color = selectedTabTextColor;
+
+document.getElementById(currentTab).style.display = "flex";
+document.getElementById(currentTab).style.visibility = "visible";
+
+
+window.onload = function () {
+    
     for (var j = 0; j < tabContents.length; j++) {
+        tabContents[j].style.visibility = "visible";
         tabContents[j].style.display = "none";
     }
-
-    currentTab = "conference";
-    initialTabSelector = "conference-tab-selector";
-
-    document.getElementById(initialTabSelector).style.backgroundColor = selectedTabBackgroundColor;
-    document.getElementById(initialTabSelector).style.color = selectedTabTextColor;
-
-    document.getElementById(currentTab).style.display = "flex";
     
+    document.getElementById(currentTab).style.display = "flex";
+    document.getElementById(currentTab).style.visibility = "visible";
 
+    
     //we take all the tab selectors (like Conference, Weekend, etc.)
     var tabSelectors = document.getElementsByClassName('tab-selectors');
 
@@ -43,6 +55,7 @@ window.onload = function () {
             var tabToView = this.getAttribute("href");
             tabToView = tabToView.substring(1);
             document.getElementById(tabToView).style.display = "flex";
+            document.getElementById(tabToView).style.visibility = "visible";
             currentTab = tabToView;
 
             var drawer = document.getElementsByClassName('mdl-layout__drawer')[0];
@@ -83,6 +96,6 @@ var clearTabSelectorsBGColor = function () {
 
     for (var i = 0; i < tabSelectors.length; i++) {
         tabSelectors[i].style.backgroundColor = unselectedTabBackgroundColor;
-        tabSelectors[i].style.color = unselectedTabTextColor;        
+        tabSelectors[i].style.color = unselectedTabTextColor;
     }
 }
